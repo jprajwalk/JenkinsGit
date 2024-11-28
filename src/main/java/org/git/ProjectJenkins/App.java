56de0aa2161c68b974218dff4e85e7fa0bd1234c
jprajwalk <prajwalkjbangalore@gmail.com>
2024-11-28 13:16:51 +0530

    new script

---
+++ src/main/java/org/git/ProjectJenkins/App.java
@@ -0,0 +1,10 @@
+package org.git.ProjectJenkins;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/git/ProjectJenkins/AppTest.java
@@ -0,0 +1,22 @@
+package org.git.ProjectJenkins;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() 
+    {
+    	System.out.println("******************************************");
+    	System.out.println("WELCOME FROM GITHUB");
+    	System.out.println("******************************************");
+    }
+}
